7cf0b89a5c5e7c6113bbba6c32b9a26353fc0f82
Nat <43274592+Cassinix@users.noreply.github.com>
2018-11-24 17:48:38 +0200

    Create fourth.js

---
+++ fourth.js
@@ -0,0 +1,34 @@
+console.clear();
+
+const parallel = (tasks, done) => {
+    const results = [];
+    if (!tasks || !tasks.length) {
+        done(results);
+    }
+    
+    let pending = tasks.length;
+    const runner = (key) => (result) => {
+        results[key] = result;
+      // we hope it is run only once
+      if(pending-- <= 1) {
+          done(results)
+      }
+    }
+    tasks.forEach(function (task, i) {
+      task(runner(i))
+    })
+};
+
+const fn = i => cb => setTimeout(() => {
+      console.log(i);
+    cb(i);
+}, 1000)
+
+// here was a bug in task
+parallel([
+    fn(1),
+  fn(2),
+  fn(3)
+], (result) => {
+    console.log('done', result)
+})
