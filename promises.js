35d744353e434428f709df2f2dde18193635c404
Nat <43274592+Cassinix@users.noreply.github.com>
2018-12-10 19:16:08 +0200

    Update promises.js

--- promises.js
+++ promises.js
@@ -1,1 +1,13 @@
-
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+      console.log('resolved');
+    }, ms);
+  });
+  
+ const promise = delay(2000);
+ promise.then(() => console.log(1));
+ promise.then(() => console.log(2));
+ promise.then(() => console.log(3));
+  
