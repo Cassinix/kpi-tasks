405dee6e93ee5f45e5192d5b257efe0d6749e581
Nat <43274592+Cassinix@users.noreply.github.com>
2018-12-14 11:30:48 +0200

    Create callbacks.js

---
+++ callbacks.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', () => {
+  document.getElementById('button').addEventListener('click', () => {
+    setTimeout(() => {
+      items.forEach(item => {
+        //your code here
+      })
+    }, 2000)
+  })
+})
